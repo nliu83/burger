f996a6644fb5de6aa6110f77f66e21288fbfc709
nliu83 <nliu8338@gmail.com>
2018-08-22 16:19:42 -0700

    creates db and config folder

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Honey mustard Bacon', false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double cheese', false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Double', false);
